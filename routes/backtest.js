028653f9f5f5302465914dafb849aecc3343a530
Varunkumar Manoharan <samvarun@gmail.com>
2026-01-07 23:55:42 +0530

    added SupertrendBearCallSpreadMainSLTPBTCController

--- routes/backtest.js
+++ routes/backtest.js
@@ -2,9 +2,13 @@ const express = require('express');
 const {
   SupertrendBullPutSpreadMainSLTPBTCController,
 } = require('../controllers/SupertrendBullPutSpreadMainSLTPBTCController');
+const {
+  SupertrendBearCallSpreadMainSLTPBTCController,
+} = require('../controllers/SupertrendBearCallSpreadMainSLTPBTCController');
 
 const router = express.Router();
 
+router.post('/btc-st-ce', SupertrendBearCallSpreadMainSLTPBTCController);
 router.post('/btc-st-pe', SupertrendBullPutSpreadMainSLTPBTCController);
 
 module.exports = router;
